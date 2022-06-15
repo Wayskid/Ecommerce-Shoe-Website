2c2cd5138f92f8aec484d48f63b7d836c22c2106
Wayskid <wayskid02@gmail.com>
2022-06-15 10:38:18 -0500

    Added Form Styling

--- javaScript/contact.js
+++ javaScript/contact.js
@@ -4,6 +4,9 @@ const bar2 = document.querySelector(".bar2")
 const bar3 = document.querySelector(".bar3")
 const cross = document.querySelector(".cross");
 const mobileNavMenu = document.querySelector(".mobileNavMenu");
+const formField = document.querySelectorAll(".formField");
+const required = document.querySelectorAll(".required");
+const labelInput = document.querySelectorAll(".labelInput");
 
 
 
@@ -14,3 +17,37 @@ burger.addEventListener("click", () => {
     bar3.classList.toggle("cross");
     mobileNavMenu.classList.toggle("showMobileMenu");
 })
+
+
+//Click To fill form
+
+
+
+// for(let a = 0; a < required.length; a++) {
+//         const ele = required[a];
+        // ele.focus();
+        // ele.addEventListener("click", () => {
+            // eleme.classList.add("inputFocus");
+            // console.log(eleme);
+            // console.log(ele);}
+    // }
+
+
+// for (let b = 0; b < labelInput.length; b++) {
+//     const eleme = labelInput[b];
+//     eleme.addEventListener("click", () => {
+//     eleme.classList.add("inputFocus");
+//     console.log(eleme);})
+
+
+// }
+
+// for (let i = 0; i < formField.length; i++) {
+//     const element = formField[i];
+    // element.addEventListener("click", () => {
+        // console.log(element);
+        // 
+
+
+        
+// }
